fb2e1fc25ebd3a9b60dc70e6dfaa0902ce4adb72
Ashley Davis <ashley_davis10419@hotmail.com>
2015-10-12 17:54:34 +0100

    Update to allow ignoring release compilation

--- cmake/FindAPG.cmake
+++ cmake/FindAPG.cmake
@@ -11,17 +11,24 @@ find_library(APG_DEBUG_LIBRARY NAMES APG-d
           DOC "The APG debug library"
 )
 
-find_library(APG_LIBRARY NAMES APG
-          DOC "The APG release library"
-)
+if ( NOT EXCLUDE_RELEASE )
+	find_library(APG_LIBRARY NAMES APG
+		  DOC "The APG release library"
+	)
+endif()
 
 if(APG_DEBUG_LIBRARY OR APG_LIBRARY)
   set(APG_FOUND TRUE)
 else()
   set(APG_FOUND FALSE)
-endif(APG_DEBUG_LIBRARY OR APG_LIBRARY)
+endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(APG DEFAULT_MSG APG_DEBUG_LIBRARY APG_LIBRARY APG_INCLUDE_DIR)
+if( NOT EXCLUDE_RELEASE) 
+	find_package_handle_standard_args(APG DEFAULT_MSG APG_DEBUG_LIBRARY APG_LIBRARY APG_INCLUDE_DIR)
+	mark_as_advanced(APG_INCLUDE_DIR APG_DEBUG_LIBRARY APG_LIBRARY)
+else ()
+	find_package_handle_standard_args(APG "(Ignored release version because EXCLUDE_RELEASE=ON)" APG_DEBUG_LIBRARY APG_INCLUDE_DIR)
+	mark_as_advanced(APG_INCLUDE_DIR APG_DEBUG_LIBRARY)
+endif()
 
-mark_as_advanced(APG_INCLUDE_DIR APG_DEBUG_LIBRARY APG_LIBRARY)
