47bc7a8cc780944e2807147fb34566e137bfa384
Ashley Davis <ashley_davis10419@hotmail.com>
2016-01-07 17:32:42 +0000

    Fix find libs + cmakelists to find tmxparser correctly

--- cmake/FindTmxParser.cmake
+++ cmake/FindTmxParser.cmake
@@ -14,7 +14,11 @@ find_library(TMXPARSER_LIBRARY NAMES tmxparser
           DOC "The tmxparser library"
 )
 
-if(TMXPARSER_LIBRARY)
+find_library(TMXPARSER_DEBUG_LIBRARY NAMES tmxparser-d
+          DOC "The tmxparser-debug library"
+)
+
+if(TMXPARSER_LIBRARY OR TMXPARSER_DEBUG_LIBRARY)
   set(TMXPARSER_FOUND TRUE)
 else()
   set(TMXPARSER_FOUND FALSE)
@@ -24,3 +28,17 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(TMXPARSER DEFAULT_MSG TMXPARSER_LIBRARY TMXPARSER_INCLUDE_DIR)
 
 mark_as_advanced(TMXPARSER_INCLUDE_DIR TMXPARSER_LIBRARY)
+
+if (TMXPARSER_DEBUG_LIBRARY AND TMXPARSER_LIBRARY)
+    message("Found tmxparser:\n\tDebug   (${TMXPARSER_DEBUG_LIBRARY})\n\tRelease (${TMXPARSER_LIBRARY})")
+    find_package_handle_standard_args(tmxparser DEFAULT_MSG TMXPARSER_DEBUG_LIBRARY TMXPARSER_LIBRARY TMXPARSER_INCLUDE_DIR)
+    mark_as_advanced(TMXPARSER_INCLUDE_DIR TMXPARSER_DEBUG_LIBRARY TMXPARSER_LIBRARY)
+elseif (TMXPARSER_DEBUG_LIBRARY)
+    message("Found tmxparser: Debug only (${TMXPARSER_DEBUG_LIBRARY})")
+    find_package_handle_standard_args(tmxparser DEFAULT_MSG TMXPARSER_DEBUG_LIBRARY TMXPARSER_INCLUDE_DIR)
+    mark_as_advanced(TMXPARSER_INCLUDE_DIR TMXPARSER_DEBUG_LIBRARY)
+elseif (TMXPARSER_LIBRARY)
+    message("Found tmxparser: Release only (${TMXPARSER_LIBRARY})")
+    find_package_handle_standard_args(tmxparser DEFAULT_MSG TMXPARSER_LIBRARY TMXPARSER_INCLUDE_DIR)
+    mark_as_advanced(TMXPARSER_INCLUDE_DIR TMXPARSER_LIBRARY)
+endif ()
